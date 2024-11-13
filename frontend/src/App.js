a11cf3fde86d204d852caaaebf7d1250452b027a
prasanth <prasanthchaandhu02@gmail.com>
2024-11-13 17:13:19 +0530

    my changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import './App.css'
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './home';
+import Login from './login';
+import SignUp from './signup';
 
 function App() {
   return (
-    <div className="App">
-        <h2>Welcome Bro</h2>
-    </div>
+    // <div className="App">
+    //     <h2>Welcome Bro</h2>
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Home() {
+    return (
+    <div>
+        <nav class="navbar bg-primary" data-bs-theme="dark">
+            
+        </nav>
+    </div>
+    )
+}
+
+export default Home;
+
